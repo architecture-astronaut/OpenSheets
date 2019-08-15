b9d4d73423f164a36f985344bf74242c51bf7ed7
Alex <alex.rhees+github@gmail.com>
2019-08-15 08:40:51 -0600

    Communication

---
+++ OpenSheets.Contracts/Commands/Communication/SendCommunication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSheets.Contracts.Commands.Communication
+{
+    public class SendCustomSms
+    {
+        
+    }
+
+    public class SendCustomEmail
+    {
+
+    }
+
+    public class SendTemplateEmail
+    {
+
+    }
+
+    public class SendTemplateSms
+    {
+
+    }
+}
---
+++ OpenSheets.Core/Communication/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSheets.Core.Communication
+{
+    public enum SmsFlags
+    {
+
+    }
+
+    public enum EmailFlags
+    {
+
+    }
+}
